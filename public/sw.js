b83e093849acfdf8f566b8e836e6fccb647bbfa3
Shailesh Patil <shailesh.patil@digital.hmrc.gov.uk>
2018-02-23 13:09:00 +0000

    Updated the home screen

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 // Load the sw-toolbox library.
 importScripts('./js/idb-keyval.js');
 
-const cacheName = 'latestNews-v5';
+const cacheName = 'latestNews-v6';
 const offlineUrl = '/offline';
 
 // Cache our known resources during install
